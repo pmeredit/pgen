281c7f2c1a30ab2506c546d7890d42a0d0534d57
Patrick Meredith <pmeredit@gmail.com>
2017-11-10 17:58:23 -0500

    Add function names as static HashSet

---
+++ src/mongo_functions.rs
@@ -0,0 +1,102 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashSet;
+
+macro_rules! set {
+     ( $( $x:expr ),* ) => {
+        {
+            let mut temp_hs = HashSet::new();
+            $(
+                temp_hs.insert($x);
+            )*
+            temp_hs
+        }
+    };
+}
+
+// Add function replacements for things we don't currently have in
+// server??
+
+// TODO: $meta
+lazy_static! {
+    static ref FUNCTIONS: HashSet<&'static str> =
+        set!["not",
+             "setEquals",
+             "setIntersection",
+             "setUnion",
+             "setDifference",
+             "setIsSubset",
+             "anyElementTrue",
+             "allElementsTrue",
+             "cmp",
+             "abs",
+             "add",
+             "ceil",
+             "divide",
+             "exp",
+             "floor",
+             "ln",
+             "log",
+             "log10",
+             "mod",
+             "multiply",
+             "pow",
+             "sqrt",
+             "subtract",
+             "trunc",
+             "concat",
+             "indexOfBytes",
+             "indexOfCP",
+             "split",
+             "strLenBytes",
+             "strLenCP",
+             "strcasecmp",
+             "substr",
+             "substrBytes",
+             "substrCP",
+             "toLower",
+             "toUpper",
+             "arrayElementAt",
+             "arrayToObject",
+             "concatArrays",
+             "filter",
+             "in",
+             "indexOfArray",
+             "isArray",
+             "map",
+             "objectToArray",
+             "range",
+             "reduce",
+             "reverseArray",
+             "size",
+             "slice",
+             "zip",
+             "literal",
+             "dayOfYear",
+             "dayOfMonth",
+             "dayOfWeek",
+             "year",
+             "month",
+             "week",
+             "hour",
+             "minute",
+             "second",
+             "millisecond",
+             "dateToString",
+             "isoDayOfWeek",
+             "isoWeek",
+             "isoWeekYear",
+             "ifNull",
+             "type",
+             "sum",
+             "avg",
+             "first",
+             "last",
+             "max",
+             "min",
+             "push",
+             "addToSet",
+             "stdDevPop",
+             "stdDevSamp"];
+}
